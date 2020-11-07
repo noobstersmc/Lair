8790544fda4806663dc10ec7bb287759a38e01a4
InfinityZ25 <henixceo@gmail.com>
2020-11-07 13:10:25 -0500

    Changes on Lair. Almost functional now

--- Lair.js
+++ Lair.js
@@ -5,17 +5,21 @@ const app = express();
 const requestify = require('requestify');
 
 //Pterodactyl, Vultr, and PORT
-const PTERO_API = "FYMUQIEAK3b8kuSWuybqYFD20NKWqga3XjdFWBcz3ogAhbbW";
-const PTERO_URL = "http://condor.jcedeno.us/";
+const PTERO_API = "pSMkBRPFJRxgsjzIXiHm6fuNqerVMQQQE6UOXb4BiVs8fio7";
+const PTERO_URL = "http://condor.jcedeno.us/api";
 const VULTR_API = "RSBCD6OMAKB6TNWS7PUBLGCLWKNTD36U7HGA";
-const PORT = 8080;
+const PORT = 420;
+//Map to keep track of Ips and Node IDS
+const map = new Map();
+//Test Values
+map.set("69", 420);
+map.set("72.184.69.100", 100)
 //Vultr Api
 const VultrNode = require('@vultr/vultr-node');
 const vultr = VultrNode.initialize({
   apiKey: VULTR_API
 }
 )
-
 //Body parse middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,52 +30,49 @@ app.post("/create-server", (req, res) => {
   }
 
 });
+//Get call to obtain self register
+app.get("/self-register", (req, res) => {
+  if (authorized(req, res)) {
+    find_self_register_from_ip(req, res);
+  }
+
+});
 //Starts the app
 app.listen(PORT, () => console.log(`Condor landed in port ${PORT}`));
+//Function to obtain self-register
+function find_self_register_from_ip(req, res) {
+  let body = req.body;
+  let ip = body.ip;
+  console.log(ip + " is being asked.");
+  let id = map.get(ip);
+  if (id == undefined) {
+    res.send({
+      error: 404
+    });
+  } else {
+    res.send({
+      for_ip: ip,
+      node_id: id,
+      command: `cd /etc/pterodactyl && sudo wings configure --panel-url ${PTERO_URL} --token ${PTERO_API} --node ${id} --override`
+    });
 
-//Auth function
-function authorized(request, respone) {
-  if (request.headers.auth != "Condor-Secreto") {
-    respone.status(401)
-    respone.send({
-      "error": "unauthorized"
-    })
-    return false;
   }
-  return true;
-}
-var vultr_request = {
-  DCID: 1,
-  OSID: 352,
-  label: "NODE",
-  SCRIPTID: 754163,
-  VPSPLANID: 202,
-  SSHKEYID:
-    "5e23537673453,5ed1290013c2e,5ed1290013c2e,5f68c34ac21bc"
 }
 //Actually create the server function
 async function create_server(request, response) {
 
-
   let creation_promise = vultr.server.create(create_vultr_json(request.body.host))
   let creation = await creation_promise;
   let id = parseInt(creation.SUBID);
-
-  //TODO: IF IP = 0.0.0.0 Then repeat until finding actual IP
   console.log(id);
-  var promise = vultr.server.list({
-    SUBID: id
-  });
 
-  //Now talk to ptero
-  let result = await promise;
-  console.log(result);
-  let ip = result.main_ip
-  console.log("IP: " + ip)
+  var ip = await obtain_ip_from_subid(id);
 
-  /*
-
-  requestify.request("http://condor.jcedeno.us/api/application/nodes", {
+  while (ip == "0.0.0.0") {
+    await sleep(1000);
+    ip = await obtain_ip_from_subid(id);
+  }
+  requestify.request(PTERO_URL + "/application/nodes", {
     method: 'POST',
     headers: {
       Authorization: `Bearer ${PTERO_API}`,
@@ -94,15 +95,39 @@ async function create_server(request, response) {
     dataType: 'json'
   }).then((res) => {
     let body = res.getBody();
-    //Somewhat make this ip relationed to this update_url in a MAP
     var update_url = body.meta.resource;
-    console.log(update_url)
+    //Save the ip in map for /self-register
+    map.set(ip, body.attributes.id);
     create_allocation_node(update_url, ip);
 
   }).catch((err) => console.log(err.getBody()));
-*/
+
   response.send(request.body)
   console.log("Recieved request")
+}
+//Auth function
+function authorized(request, respone) {
+  if (request.headers.auth != "Condor-Secreto") {
+    respone.status(401)
+    respone.send({
+      "error": "unauthorized"
+    })
+    return false;
+  }
+  return true;
+}
+async function obtain_ip_from_subid(id) {
+  var promise = vultr.server.list({
+    SUBID: id
+  });
+  let result = await promise;
+  let coso = result.main_ip
+  return coso;
+}
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
 function create_allocation_node(url, ipv4) {
   requestify.request(`${url}/allocations`, {
