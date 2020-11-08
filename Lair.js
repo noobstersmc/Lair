09466ce10a4d2fb5a35d08341c4bad4033f5d46d
InfinityZ25 <henixceo@gmail.com>
2020-11-08 12:07:31 -0500

    Added /vultr/server/v2 route

--- Lair.js
+++ Lair.js
@@ -2,7 +2,7 @@ const { urlencoded } = require("express");
 const express = require("express");
 const path = require("path");
 const app = express();
-const requestify = require('requestify');
+const requestify = require("requestify");
 
 //Pterodactyl, Vultr, and PORT
 const PTERO_API = "pSMkBRPFJRxgsjzIXiHm6fuNqerVMQQQE6UOXb4BiVs8fio7";
@@ -13,51 +13,108 @@ const PORT = 420;
 const map = new Map();
 //Test Values
 map.set("69", 420);
-map.set("72.184.69.100", 100)
+map.set("72.184.69.100", 100);
 //Vultr Api
-const VultrNode = require('@vultr/vultr-node');
+const VultrNode = require("@vultr/vultr-node");
 const vultr = VultrNode.initialize({
-  apiKey: VULTR_API
-}
-)
+  apiKey: VULTR_API,
+});
 //Body parse middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //Should allow for data to be written.
 app.post("/create-server", (req, res) => {
   if (authorized(req, res)) {
-    create_server(req, res)
+    create_server(req, res);
   }
-
 });
 //Get call to obtain self register
 app.get("/self-register", (req, res) => {
   if (authorized(req, res)) {
     find_self_register_from_ip(req, res);
   }
-
 });
 app.get("/vultr/sizes", (req, res) => {
-  console.log("Processing request of vultr sizes...")
+  console.log("Processing request of vultr sizes...");
   get_sizes().then((result) => {
     res.send(result);
-    console.log("Done.")
+    console.log("Done.");
   });
 });
 app.get("/vultr/locations", (req, res) => {
-  console.log("Recieved request of locations list")
-  vultr.regions.list().then((result)=>{
+  console.log("Recieved request of locations list");
+  vultr.regions.list().then((result) => {
     res.send(result);
-    console.log("Done.")
+    console.log("Done.");
   });
 });
 app.get("/vultr/servers", (req, res) => {
-  console.log("Recieved request of active server list")
-  vultr.server.list().then((result)=>{
+  console.log("Recieved request of active server list");
+  vultr.server.list().then((result) => {
     res.send(result);
-    console.log("Done.")
+    console.log("Done.");
   });
 });
+let vultr_api_url = "https://api.vultr.com/v2/";
+app.get("/vultr/servers/v2", (req, res) => {
+  console.log("Recieved request of active server list");
+  //Call to the vultr v2 api rest
+  let input = req.query.ip;
+  console.log(input);
+  if(input == null){
+    res.sendStatus(400);
+  }
+
+  requestify
+    .request(`${vultr_api_url}instances`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${VULTR_API}`,
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: {},
+      dataType: "json",
+    })
+    .then((result) => {
+      let response_body = result.getBody();
+      let total_instances = parseInt(response_body.meta.total);
+      let instances = response_body.instances;
+
+      let cosos = {
+        total: total_instances,
+        instances: instances,
+      };
+
+      console.log(cosos);
+
+      
+      let response = {
+        from_ip: '',
+        id:'',
+        command: ''
+      }
+
+      instances.forEach((instance) => {
+        if (instance.main_ip == input) {
+          
+          response.from_ip = instance.main_ip;
+          response.id = instance.id
+          response.command = `cv add ${instance.id} ${instance.main_ip} 25565`
+
+          console.log('ID Found');
+
+          res.send(response);
+          return;
+        }
+      });
+      console.log(req.params.ip);
+
+      res.sendStatus(404);
+    });
+});
+//
+
 //Starts the app
 app.listen(PORT, () => console.log(`Condor landed in port ${PORT}`));
 //Async function to get vultr available sizes
@@ -74,21 +131,21 @@ function find_self_register_from_ip(req, res) {
   let id = map.get(ip);
   if (id == undefined) {
     res.send({
-      error: 404
+      error: 404,
     });
   } else {
     res.send({
       for_ip: ip,
       node_id: id,
-      command: `cd /etc/pterodactyl && sudo wings configure --panel-url ${PTERO_URL} --token ${PTERO_API} --node ${id} --override`
+      command: `cd /etc/pterodactyl && sudo wings configure --panel-url ${PTERO_URL} --token ${PTERO_API} --node ${id} --override`,
     });
-
   }
 }
 //Actually create the server function
 async function create_server(request, response) {
-
-  let creation_promise = vultr.server.create(create_vultr_json(request.body.host))
+  let creation_promise = vultr.server.create(
+    create_vultr_json(request.body.host)
+  );
   let creation = await creation_promise;
   let id = parseInt(creation.SUBID);
   console.log(id);
@@ -99,56 +156,60 @@ async function create_server(request, response) {
     await sleep(1000);
     ip = await obtain_ip_from_subid(id);
   }
-  requestify.request(PTERO_URL + "api/application/nodes", {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${PTERO_API}`,
-      "Content-Type": "application/json",
-      Accept: "application/json"
-    },
-    body: {
-      name: `${request.body.host}`,
-      location_id: 1,
-      fqdn: ip,
-      scheme: 'http',
-      memory: 1000,
-      disk: 100000,
-      disk_overallocate: 0,
-      memory_overallocate: 0,
-      upload_size: 100,
-      daemon_sftp: 2022,
-      daemon_listen: 8080,
-    },
-    dataType: 'json'
-  }).then((res) => {
-    let body = res.getBody();
-    var update_url = body.meta.resource;
-    //Save the ip in map for /self-register
-    map.set(ip, body.attributes.id);
-    create_allocation_node(update_url, ip);
+  requestify
+    .request(PTERO_URL + "api/application/nodes", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${PTERO_API}`,
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: {
+        name: `${request.body.host}`,
+        location_id: 1,
+        fqdn: ip,
+        scheme: "http",
+        memory: 1000,
+        disk: 100000,
+        disk_overallocate: 0,
+        memory_overallocate: 0,
+        upload_size: 100,
+        daemon_sftp: 2022,
+        daemon_listen: 8080,
+      },
+      dataType: "json",
+    })
+    .then((res) => {
+      let body = res.getBody();
+      var update_url = body.meta.resource;
+      //Save the ip in map for /self-register
+      map.set(ip, body.attributes.id);
+      create_allocation_node(update_url, ip);
+    })
+    .catch((err) => console.log(err.getBody()));
 
-  }).catch((err) => console.log(err.getBody()));
-
-  response.send(request.body)
-  console.log(`Creating server for ${request.body.displayname} of type ${request.body.game_type} and seed ${request.body.extra_data.level_seed}`)
+  response.send(request.body);
+  console.log(
+    `Creating server for ${request.body.displayname} of type ${request.body.game_type} and seed ${request.body.extra_data.level_seed}`
+  );
 }
 //Auth function
 function authorized(request, respone) {
   if (request.headers.auth != "Condor-Secreto") {
-    respone.status(401)
+    respone.status(401);
     respone.send({
-      "error": "unauthorized"
-    })
+      error: "unauthorized",
+    });
     return false;
   }
   return true;
 }
 async function obtain_ip_from_subid(id) {
   var promise = vultr.server.list({
-    SUBID: id
+    SUBID: id,
   });
   let result = await promise;
-  let coso = result.main_ip
+  let coso = result.main_ip;
   return coso;
 }
 function sleep(ms) {
@@ -157,23 +218,24 @@ function sleep(ms) {
   });
 }
 function create_allocation_node(url, ipv4) {
-  requestify.request(`${url}/allocations`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${PTERO_API}`,
-      "Content-Type": "application/json",
-      Accept: "application/json"
-    },
-    body: {
-      ip: ipv4,
-      ports: [
-        "25565", "8081"
-      ]
-    },
-    dataType: 'json'
-  }).then((res) => {
-    console.log("Reponse = " + res.code)
-  }).catch((err) => console.log(err.getBody()));
+  requestify
+    .request(`${url}/allocations`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${PTERO_API}`,
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: {
+        ip: ipv4,
+        ports: ["25565", "8081"],
+      },
+      dataType: "json",
+    })
+    .then((res) => {
+      console.log("Reponse = " + res.code);
+    })
+    .catch((err) => console.log(err.getBody()));
 }
 
 function create_vultr_json(name) {
@@ -183,9 +245,6 @@ function create_vultr_json(name) {
     label: name,
     SCRIPTID: 754163,
     VPSPLANID: 403,
-    SSHKEYID:
-      "5e23537673453,5ed1290013c2e,5ed1290013c2e,5f68c34ac21bc"
-  }
-
+    SSHKEYID: "5e23537673453,5ed1290013c2e,5ed1290013c2e,5f68c34ac21bc",
+  };
 }
-
