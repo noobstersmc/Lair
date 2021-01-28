3f6a170ebf6a69ebb5db0aca24aa3a51726d4ccb
Juan Cedeno <37850007+InfinityZ25@users.noreply.github.com>
2021-01-28 16:49:40 -0500

    Update VultrDriver.js

--- src/VultrDriver.js
+++ src/VultrDriver.js
@@ -5,9 +5,9 @@ const vultr = VultrNode.initialize({
 });
 
 let uhc_run_url =
-    "https://gist.githubusercontent.com/InfinityZ25/747362f81193e386015fac7515304ee8/raw/0ad16a99b63bfb99510e65a2f1ad352313176dc3/uhc-run-install.sh",
+    "https://gist.githubusercontent.com/InfinityZ25/747362f81193e386015fac7515304ee8/raw/04316a8e70073d99edd9ccfb39cd0ad1a3204c39/uhc-run-install.sh",
   uhc_url =
-    "https://gist.github.com/InfinityZ25/747362f81193e386015fac7515304ee8/raw/0ad16a99b63bfb99510e65a2f1ad352313176dc3/uhc-install.sh";
+    "https://gist.githubusercontent.com/InfinityZ25/747362f81193e386015fac7515304ee8/raw/04316a8e70073d99edd9ccfb39cd0ad1a3204c39/uhc-install.sh";
 
 /**
  * Creates a server instance with the given paramters.
